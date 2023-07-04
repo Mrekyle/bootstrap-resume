7e4c98af88f1556bc2439355fd91d05e92062de9
Kyle Chart <64297454+Mrekyle@users.noreply.github.com>
2023-07-04 12:57:19 +0100

    Edit fetchGitHubInformation function to receive a second response of the repo data

--- assets/js/github.js
+++ assets/js/github.js
@@ -41,11 +41,14 @@ function fetchGitHubInformation (event) {
             <img style="padding-top: 20px" src="assets/css/loader.gif" alt="Loading the user" />
         </div>`)
     $.when(
-        $.getJSON(`https://api.github.com/users/${username}`) // Accessing the apis address to find the username that has been requested
+        $.getJSON(`https://api.github.com/users/${username}`), // Accessing the apis address to find the username that has been requested
+        $.getJSON(`https://api.github.com/users/${username}/repos`)
     ).then(
-        function (response) { 
-            var userData = response; // Storing the response in the variable to allow us to display that on the webpage easier 
+        function (firstResponse, secondResponse) { 
+            var userData = firstResponse[0]; // Storing the response in the variable to allow us to display that on the webpage easier 
+            var userRepoData = secondResponse[0];
             $('#gh-user-data').html(userInformationHtml(userData))
+            $('#gh-repo-data').html(repoInformationHtml(userRepoData))
         }, function(errorResponse) { // Incase the user doest exist or there was a problem, We then set the html to display an error message to the user of the page
             if(errorResponse.status === 404) { // Checking the error status that was returned from the api to the webpage
                 $('#gh-user-data').html(
